138def24309c44c9b7ecc6797779f1e4422d8e76
Zilong-Li <zimusen94@gmail.com>
2019-07-04 16:24:42 +0800

    modify test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-time ../bin/BaseVarC basetype -t 4 -b 10 -i data/bam.list -s chr17:41197700-41276155 -r data/chr17.fa.gz -o test.out >test.sh.o 2>test.sh.e
+time ../bin/BaseVarC basetype -t 4 -b 10 -i bam.list -s chr17:41197700-41276155 -r data/chr17.fa.gz -o test.out >test.sh.o 2>test.sh.e
